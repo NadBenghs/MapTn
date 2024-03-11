d4ac278f79fc9a7abd9704f3d6ebdf6f909be3fb
NadBenghs <naderbenghachem2002@gmail.com>
2024-03-11 11:03:37 +0000

    first commit

---
+++ Map/script.js
@@ -0,0 +1,260 @@
+const Gov = {
+    Tataouine: {
+       chefLieu: "Tataouine",
+       superficie: 38889 ,
+       population: 310000,
+       densitePopulation: 9,
+       description:"Tataouine, région du sud-est, est célèbre pour ses paysages désertiques et ses anciennes ksour. C'est une région qui a attiré l'attention grâce à son rôle dans la saga Star Wars.",
+
+
+    },
+
+    Kebili: {
+        chefLieu: "Kebili",
+        superficie:22454 ,
+        population:250000,
+        densitePopulation:5,
+        description:"Kébili, une autre région du sud, est connue pour ses dunes de sable, ses palmeraies et son ambiance saharienne. Elle offre une expérience authentique du désert.",
+ 
+ 
+     },
+
+    Tozeur: {
+        chefLieu: "Tozeur",
+        superficie: 4719,
+        population: 160000,
+        densitePopulation:6,
+        description:" Tozeur, dans le sud-ouest, est célèbre pour son oasis, son architecture traditionnelle et ses paysages désertiques. C'est une destination touristique unique.",
+ 
+ 
+     },
+
+     Medenine: {
+        chefLieu: "Medenine",
+        superficie:9167 ,
+        population: 460000,
+        densitePopulation:29,
+        description:"Médenine, avec ses ghorfas (entrepôts traditionnels), fait partie du patrimoine architectural de la Tunisie. C'est une région qui allie histoire et tradition."
+
+
+ 
+     
+     },
+     Tunis: {
+        chefLieu: "Tunis",
+        superficie:300 ,
+        population: "1200000",
+        densitePopulation:2544,
+        description:" La capitale de la Tunisie, Tunis, est un mélange fascinant de culture ancienne et de modernité. Avec sa médina historique et ses avenues animées, elle est le cœur économique et politique du pays.",
+ 
+ 
+     },
+     Ariana: {
+        chefLieu: "Ariana",
+        superficie:482,
+        population: 600000,
+        densitePopulation:3036,
+        description:"Ariana, gouvernorat voisin de Tunis, est connu pour son ambiance urbaine, ses institutions éducatives et ses quartiers résidentiels paisibles.",
+ 
+ 
+     },
+     Ben_Arous: {
+        chefLieu: "Ben Arous",
+        superficie:210 ,
+        population: 630000,
+        densitePopulation:3172,
+        description:" En bordure de la mer Méditerranée, Ben Arous abrite des villes industrielles telles que Ben Arous et Mornag, ainsi que des zones côtières attrayantes.",
+ 
+ 
+     },
+     La_Manouba: {
+        chefLieu: "La Manouba",
+        superficie:288 ,
+        population: 400000,
+        densitePopulation:1393,
+        description:": La Manouba est réputée pour son université et ses quartiers résidentiels. Elle offre un équilibre entre l'éducation, la culture et la vie quotidienne.",
+ 
+ 
+     },
+     Nabeul: {
+        chefLieu: "Nabeul",
+        superficie:2353 ,
+        population: 850000,
+        densitePopulation:362,
+        description:" Niché sur la côte nord-est, Nabeul est célèbre pour ses plages de sable fin, ses marchés colorés et son industrie florissante de production d'agrumes.",
+ 
+ 
+     },
+     Zaghouan: {
+        chefLieu: "Zaghouan",
+        superficie:2768 ,
+        population: 180000,
+        densitePopulation:74,
+        description:"Connue pour ses sources d'eau et son histoire ancienne, Zaghouan est une destination où la nature rencontre la culture, offrant des vues panoramiques depuis le mont Zaghouan.",
+ 
+ 
+     },
+     Bizerte: {
+        chefLieu: "Bizerte",
+        superficie:3750 ,
+        population: 650000,
+        densitePopulation:254,
+        description:"Avec son port maritime vital, Bizerte est une ville portuaire animée. Son histoire riche et ses plages en font une destination touristique populaire.",
+ 
+ 
+     },
+     Beja: {
+        chefLieu: "Beja",
+        superficie:3740,
+        population: 310000,
+        densitePopulation:53,
+        description:" Béja, entourée de collines verdoyantes, est réputée pour son histoire romaine et son ambiance tranquille. Le site archéologique de Dougga attire les amateurs d'histoire.",
+ 
+ 
+     },
+     Jendouba: {
+        chefLieu: "Jendouba",
+        superficie:7791 ,
+        population: 400000,
+        densitePopulation:53,
+        description:"Située dans le nord-ouest, Jendouba est caractérisée par ses montagnes et ses forêts. La région est propice à la randonnée et à la découverte de la nature.",
+ 
+ 
+     },
+     Le_Kef :{
+        chefLieu: "Le Kef",
+        superficie:4965,
+        population: 310000,
+        densitePopulation:44,
+        description:"Le Kef, avec son impressionnant site médiéval, est un trésor historique. Ses collines boisées ajoutent une beauté naturelle à cette région.",
+ 
+ 
+     },
+     Siliana: {
+        chefLieu: "Siliana",
+        superficie:4749 ,
+        population: 220000,
+        densitePopulation:45,
+        description:" Siliana, située dans le nord-ouest de la Tunisie, est caractérisée par ses paysages montagneux. Elle offre une atmosphère paisible, propice à la détente et à la découverte de la nature.",
+ 
+ 
+     },
+     Kairouan: {
+        chefLieu: "Kairouan",
+        superficie:6712 ,
+        population:640000,
+        densitePopulation:105,
+        description:"Kairouan, ville sainte de l'islam, est réputée pour ses monuments historiques, dont la Grande Mosquée et la Médina. C'est un centre culturel et spirituel important.",
+ 
+ 
+     },
+     Kasserine: {
+        chefLieu: "Kasserine",
+        superficie:8260 ,
+        population:440000,
+        densitePopulation:40,
+        description:"Kasserine, au centre-ouest, est marquée par ses montagnes et son histoire ancienne. C'est une région qui a joué un rôle significatif lors de divers événements historiques.",
+ 
+ 
+     },
+     Sidi_Bouzid: {
+        chefLieu: "Sidi Bouzid",
+        superficie:6360 ,
+        population:450000,
+        densitePopulation:77,
+        description:"Connue comme le berceau de la révolution tunisienne en 2010-2011, Sidi Bouzid est une région agricole importante avec une histoire moderne et un impact significatif.",
+ 
+ 
+     },
+     Sousse: {
+        chefLieu: "Sousse",
+        superficie:2669,
+        population: 720000,
+        densitePopulation:311,
+        description:"Sousse, sur la côte est, est une ville balnéaire réputée pour sa médina préservée et ses plages. C'est également un centre touristique majeur.",
+ 
+ 
+     },
+     Monastir: {
+        chefLieu: "Monastir",
+        superficie:1018,
+        population: 590000,
+        densitePopulation:275,
+        description:" Monastir, avec son ribat et son mausolée de Bourguiba, est une ville historique sur la côte est. Elle allie histoire, culture et tourisme.",
+ 
+ 
+     },
+     Mahdia: {
+        chefLieu: "Mahdia",
+        superficie:2966 ,
+        population: 420000,
+        densitePopulation:122,
+        description:"Mahdia, une ville portuaire au charme méditerranéen, est connue pour sa médina pittoresque et ses plages de sable fin. C'est un lieu de détente et de découverte.",
+ 
+ 
+     },
+     Sfax: {
+        chefLieu: "Sfax",
+        superficie:2894 ,
+        population: 955000,
+        densitePopulation:276,
+        description:"Sfax, deuxième plus grande ville de Tunisie, est un important centre économique et industriel. Son histoire, sa médina et son activité portuaire en font une ville dynamique.",
+ 
+ 
+     },
+     Gafsa: {
+        chefLieu: "Gafsa",
+        superficie:7032,
+        population: "340000",
+        densitePopulation:"22",
+        description:"Gafsa, au sud-ouest, est renommée pour ses ressources minières et son riche patrimoine archéologique, notamment les vestiges romains.",
+ 
+ 
+     },
+     Gabes: {
+        chefLieu: "Gabes",
+        superficie:7166 ,
+        population:420000,
+        densitePopulation:48,
+        description:"Gabès, sur la côte sud-est, est connue pour son industrie pétrochimique et ses oasis. La région offre un mélange d'activités industrielles et de beautés naturelles.",
+ 
+ 
+     }
+}
+
+function showInformation(gov){
+
+ event.preventDefault();
+
+var titre=document.getElementById("map_title");
+var guide=document.getElementById("map_guide");
+titre.textContent="";
+guide.textContent="";
+
+var gouv=gov.alt;
+var titregouv=document.getElementById("title_gouv");
+var chefLieu=document.getElementById("chef_lieu");
+var superficie=document.getElementById("superficie");
+var densitePopulation=document.getElementById("densite");
+var description=document.getElementById("description");
+var population=document.getElementById("population");
+var ChosenGov=Gov[gouv];
+titregouv.textContent= ChosenGov.chefLieu;
+chefLieu.textContent="Chef-Lieu : " + ChosenGov.chefLieu;
+superficie.textContent="Superficie en km2: " + ChosenGov.superficie;
+population.textContent="Population : " + ChosenGov.population;
+densitePopulation.textContent="Densité de population par km2 : " + ChosenGov.densitePopulation;
+description.textContent="Description :" + ChosenGov.description;
+
+
+}
+
+
+function showGouv(gov)
+{
+
+var gouv=gov.title;
+var Nom_gouv=document.getElementById("nom_gouv");
+Nom_gouv.textContent="Vous survolez actuellement : "+ gouv;
+
+}
